2f033871c035b6ff02b28a2aeec0a822701db0dc
ignacioreyesfs <ignacioreyesfs@gmail.com>
2022-07-30 10:31:23 -0300

    initial spring boot project

---
+++ src/test/java/com/ireyes/findMyPet/FindMyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ireyes.findMyPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
